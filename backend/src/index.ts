0f7fc7659102dc94fde052cc194e5f474d7c51e5
bodatero <bodiamykhats@gmail.com>
2025-10-26 13:53:51 +0200

    pipeline changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,8 +15,6 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 const initializeApp = async () => {
-  console.log("Database is created");
-
   AppDataSource.initialize().then(async (dataSource) => {
     console.log("✅ Database connected!");
 
@@ -61,20 +59,21 @@ const initializeApp = async () => {
 
 console.log("current env", process.env.NODE_ENV);
 console.log("DB_URL", process.env.DB_URL);
+console.log("DB_URL", process.env.TEST_VAR);
 
-// if (process.env.NODE_ENV === "production") {
-//   initializeApp();
-// } else {
-//   console.log("Checking database existence");
-//   createDatabase({
-//     ifNotExist: true,
-//     options: {
-//       type: "postgres",
-//       host: process.env.DB_HOST || "localhost",
-//       port: Number(process.env.DB_PORT) || 5432,
-//       username: process.env.DB_USER || "postgres",
-//       password: process.env.DB_PASSWORD || "postgres",
-//       database: process.env.DB_NAME || "reviews_db",
-//     },
-//   }).then(initializeApp);
-// }
+if (process.env.NODE_ENV === "production") {
+  initializeApp();
+} else {
+  console.log("Checking database existence");
+  createDatabase({
+    ifNotExist: true,
+    options: {
+      type: "postgres",
+      host: process.env.DB_HOST || "localhost",
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres",
+      database: process.env.DB_NAME || "reviews_db",
+    },
+  }).then(initializeApp);
+}
